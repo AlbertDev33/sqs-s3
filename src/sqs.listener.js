f4bf8455790592604fcec5352b988605f66b1c1a
Albert <albert.rocha@r2t.company>
2022-04-10 16:47:30 -0300

    feature: sqs function implementation

--- src/sqs.listener.js
+++ src/sqs.listener.js
@@ -1,6 +1,15 @@
 class Handler {
     async main(event) {
-        console.log('event', JSON.stringify(event, null, 2));
+        const [{ body, messageId }] = event.Records;
+        const item = JSON.parse(body);
+        console.log('event', JSON.stringify({
+            ...item,
+            messageId,
+            at: new Date().toISOString()
+        },
+            null,
+            2
+        ));
         try {
             return {
                 statusCode: 200,
