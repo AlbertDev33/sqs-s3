0783d4efed0906960c42b3f7330efe5d039f4fbe
Albert <albert.rocha@r2t.company>
2022-04-10 14:53:09 -0300

    feature: s3 function

--- src/s3.listener.js
+++ src/s3.listener.js
@@ -1,5 +1,44 @@
+const AWS = require('aws-sdk');
+
 class Handler {
+    constructor({ s3Svc, sqsSvc }) {
+        this.s3Svc = s3Svc;
+        this.sqsSvc = sqsSvc;
+    }
+
+    static getSdks() {
+        const host = process.env.LOCALSTACK_HOST || 'localhost';
+        const s3Port = process.env.S3_PORT || '4566';
+        const sqsPort = process.env.SQS_PORT || '4576';
+        const isLocal = process.env.IS_LOCAL;
+        const s3Endpoint = new AWS.Endpoint(
+            `http://${host}:${s3Port}`
+        );
+        const s3Config = {
+            endpoint: s3Endpoint,
+            s3ForcePathStyle: true
+        };
+
+        const sqsEndpoint = new AWS.Endpoint(
+            `http://${host}:${sqsPort}`
+        );
+        const sqsConfig = {
+            endpoint: sqsEndpoint
+        }
+
+        if (!isLocal) {
+            delete s3Config.endpoint,
+            delete sqsConfig.endpoint
+        }
+
+        return {
+            s3: new AWS.S3(s3Config),
+            sqs: new AWS.SQS(sqsConfig)
+        }
+    }
+
     async main(event) {
+        console.log('event', JSON.stringify(event, null, 2));
         try {
             return {
                 statusCode: 200,
@@ -14,6 +53,9 @@ class Handler {
         }
     }
 }
-
-const handler = new Handler();
+const { s3, sqs } = Handler.getSdks();
+const handler = new Handler({
+    s3Svc: s3,
+    sqsSvc: sqs
+});
 module.exports = handler.main.bind(handler);
